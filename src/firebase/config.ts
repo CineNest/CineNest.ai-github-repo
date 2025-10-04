738d7b452bf4d238962b54f6d9fd5539cb3911ea
Nest <teamnestai@gmail.com>
2025-10-04 05:32:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1285665205-cddf9",
+  "appId": "1:74981173131:web:1587a729faade06abe718e",
+  "apiKey": "AIzaSyC5_eJmt18jNXKurewAsO216pgzYfPlYcc",
+  "authDomain": "studio-1285665205-cddf9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "74981173131"
+};
